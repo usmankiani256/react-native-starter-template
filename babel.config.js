0af42f09d8363858d2ccefcdc06c2ab150009dc1
Usman Kiani <usmankiani256@gmail.com>
2021-08-23 03:08:10 +0500

    :sparkles: add module-resolver paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@assets': './app/assets',
+          '@images': './app/assets/images',
+          '@components': './app/components',
+          '@navigation': './app/navigation',
+          '@screens': './app/screens',
+          '@services': './app/services',
+          '@state': './app/state',
+          '@theme': './app/theme',
+        },
+      },
+    ],
+  ],
+}
