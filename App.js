38973aba3251abdb3f0eb10b52a41f0d9b28e05d
Usman Kiani <usmankiani256@gmail.com>
2021-08-24 03:17:20 +0500

    :sparkles: add rn-paper provider to root

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import RootNavigator from '@navigation'
+
 import { Provider as ReduxProvider } from 'react-redux'
 import { store } from '@redux'
+
+import { Provider as PaperProvider } from 'react-native-paper'
+import { PaperTheme } from '@theme'
 
 const App = () => {
   return (
     <ReduxProvider store={store}>
-      <RootNavigator />
+      <PaperProvider theme={PaperTheme}>
+        <RootNavigator />
+      </PaperProvider>
     </ReduxProvider>
   )
 }
