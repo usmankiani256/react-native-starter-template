bf34480676b913b328190918fb5b0bcbacdded13
Usman Kiani <usmankiani256@gmail.com>
2021-08-24 02:38:56 +0500

    :sparkles: add redux provider to root

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import RootNavigator from '@navigation'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from '@redux'
 
 const App = () => {
-  return <RootNavigator />
+  return (
+    <ReduxProvider store={store}>
+      <RootNavigator />
+    </ReduxProvider>
+  )
 }
 
 export default App
